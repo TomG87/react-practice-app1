8f39ac6ad7716c429ba9c7e6b62a9bc4f4c0ad32
Tom Gagliano <tgaglianoll@gmail.com>
2024-03-08 22:55:37 -0500

    add title, .list marker classNames and further css properties to stylize static page

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MainContent() {
   return (
-    <div>
-      <h1>Fun facts about React </h1>
+    <main>
+      <h1 className="title">Fun facts about React </h1>
       <ul className="list">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
@@ -11,6 +11,6 @@ export default function MainContent() {
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
       </ul>
-    </div>
+    </main>
   );
 }
